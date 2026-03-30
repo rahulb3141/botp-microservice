93f438d5020c09675a51262e7fd4df5a74315ffb
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-03-30 15:26:09 +0530

    Update variables.tf

--- terraform/green/variables.tf
+++ terraform/green/variables.tf
@@ -1,1 +1,22 @@
+variable "region" {
+  description = "AWS region for Blue environment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR for Blue environment"
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = [
+    "10.20.1.0/24",
+    "10.20.2.0/24"
+  ]
+}
+
 
