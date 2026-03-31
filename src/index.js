e1c348eab1c5a350c6e67176d62bfc5b1dfb13c6
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-03-31 15:53:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello from Blue-Green Deployment! This is the active version.");
+});
+
+app.listen(8080, () => {
+    console.log("App running on port 8080");
+});
